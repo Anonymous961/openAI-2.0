4bfd6663c7bfec6a0c29e2800f8a51a14b8f2bce
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-07-04 20:53:07 +0530

    check 2

--- client/src/pages/check2.jsx
+++ client/src/pages/check2.jsx
@@ -1,8 +1,8 @@
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 
 const Check2 = () => {
-  const params = useParams();
-  console.log("checking if check 2 is working" + params.id);
+  // const params = useParams();
+  // console.log("checking if check 2 is working" + params.id);
   return <div>this is check 2</div>;
 };
 
